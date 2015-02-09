55a10f7521d52ed94071268f12c444f2a34614fc
Tom MacWright <tom@macwright.org>
2015-02-09 15:01:04 -0500

    Late-config

--- src/index.js
+++ src/index.js
@@ -1,22 +1,24 @@
 Object.assign = require('object-assign');
-var React = require('react');
-var Router = require('react-router');
-var config = require('./config.js');
-var { Route, DefaultRoute, RouteHandler } = Router;
-var Events = require('./components/rows.js');
-
-var App = React.createClass({
-  render: function () {
-    return (
-      <div>
-        <RouteHandler />
-      </div>
-    );
-  }
-});
 
 module.exports = function(element, inputConfig) {
+
+  var config = require('./config.js');
   Object.assign(config, inputConfig);
+
+  var React = require('react');
+  var Router = require('react-router');
+  var { Route, DefaultRoute, RouteHandler } = Router;
+  var Events = require('./components/rows.js');
+
+  var App = React.createClass({
+    render: function () {
+      return (
+        <div>
+          <RouteHandler />
+        </div>
+      );
+    }
+  });
 
   var routes = (
     <Route handler={App} path='/'>
