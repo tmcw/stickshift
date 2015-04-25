26f58660c30678b2fd1c126e7c97b57ab216b585
Tom MacWright <tom@macwright.org>
2015-04-25 17:43:16 -0400

    Allow client to be function instead of endpoint, add standalone build

--- src/client.js
+++ src/client.js
@@ -15,15 +15,19 @@ module.exports.deleteQuery = deleteQuery;
 module.exports.list = function(cb) { return db.list(cb); };
 
 function runQuery(query, callback) {
+  if (typeof config.endpoint === 'string') {
     xhr({
-        uri: config.endpoint,
-        method: 'POST',
-        json: {
-            query: query
-        },
+      uri: config.endpoint,
+      method: 'POST',
+      json: {
+        query: query
+      },
     }, (err, res, body) => {
-        callback(err, body);
+      callback(err, body);
     });
+  } else if (typeof config.endpoint === 'function') {
+    config.endpoint(query, callback);
+  }
 }
 
 function saveQuery(name, query, callback) {
